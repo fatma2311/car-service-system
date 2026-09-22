58bcd83aab87ded71074b1f14672c1f0f4bdd9a9
Fatma <batootmohamed916@gmail.com>
2026-09-22 21:57:31 +0300

    Add Technician, Maintenance, and Appointment views with navigation

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.vehicle_list, name='vehicle_list'),
+    path('technicians/', views.technician_list, name='technician_list'),
+    path('maintenance/', views.maintenance_list, name='maintenance_list'),
+    path('appointments/', views.appointment_list, name='appointment_list'),
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from .models import Vehicle
-
+from .models import Vehicle, Technician, MaintenanceRecord, Appointment
 
 def vehicle_list(request):
     vehicles = Vehicle.objects.all()
     return render(request, 'vehicles/vehicle_list.html', {'vehicles': vehicles})
+
+
+def technician_list(request):
+    technicians = Technician.objects.all()
+    return render(request, 'vehicles/technician_list.html', {'technicians': technicians})
+
+
+def maintenance_list(request):
+    records = MaintenanceRecord.objects.all().order_by('-service_date')
+    return render(request, 'vehicles/maintenance_list.html', {'records': records})
+
+
+def appointment_list(request):
+    appointments = Appointment.objects.all().order_by('scheduled_date')
+    return render(request, 'vehicles/appointment_list.html', {'appointments': appointments})
+
